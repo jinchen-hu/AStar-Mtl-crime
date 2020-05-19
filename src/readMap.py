0a57998e4fbc722b8aaaca34e2931ca8793394b1
jinchen-hu <jinchenhu.luke@gmail.com>
2020-05-19 17:20:02 -0400

    test

--- src/readMap.py
+++ src/readMap.py
@@ -1,9 +1,10 @@
 import shapefile
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import geopandas as gpd
+import numpy as np
 
 shpFilePath='../shape_file/crime_dt.shp'
-
+'''
 shape = shapefile.Reader(shpFilePath, encoding='ISO-8859-1')
 shapeRecords = shape.shapeRecords()
 
@@ -16,3 +17,12 @@ for i in range(len(shapeRecords)):
 plt.show()
 for i in range(10):
     print(shapeRecords[i].shape.__geo_interface__['coordinates'])
+'''
+
+mtr = gpd.GeoDataFrame.from_file(shpFilePath)
+ar = mtr.loc[:,'geometry'][7].__geo_interface__['coordinates']
+print(len(mtr.loc[:,'geometry']))
+print(ar[1])
+print(ar)
+#print(mtr.__geo_interface__('coordinates'))
+
