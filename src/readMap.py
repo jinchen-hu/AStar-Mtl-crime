525d60bfd9b925aa9f7a36bed8f6a5da4fc8b6ba
jinchen-hu <jinchenhu.luke@gmail.com>
2020-05-25 19:30:51 -0400

    update

--- src/readMap.py
+++ src/readMap.py
@@ -3,26 +3,42 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 import numpy as np
 
+# Define the path of shape files
 shpFilePath='../shape_file/crime_dt.shp'
-'''
-shape = shapefile.Reader(shpFilePath, encoding='ISO-8859-1')
-shapeRecords = shape.shapeRecords()
+# Read the data of shape file as GeoDataFrame list, and extract geometry object only
+mtr_data = gpd.GeoDataFrame.from_file(shpFilePath).geometry
+# Get the length of the list
+size = len(mtr_data)
+# X stores x-axis data, Y stores y-axis data
+X = mtr_data.x
+Y = mtr_data.y
 
-for i in range(len(shapeRecords)):
-    x = shapeRecords[i].shape.__geo_interface__["coordinates"][0]
-    y = shapeRecords[i].shape.__geo_interface__["coordinates"][1]
-    plt.plot(x, y)
+# Define the range of map
+grid_size = 0.002
+X0, X1 = -73.59, -73.55
+Y0, Y1 = 45.49, 45.53
+# Compute grids, in case the grid_size cannot be divided evenly, plus half grad size is needed
+x_axis = np.arange(X0, X1 + grid_size/2, grid_size)
+y_axis = np.arange(Y0, Y1 + grid_size/2, grid_size)
+# Create a 2d histogram to collect criminals
+crim_data, x_edges, y_edges = np.histogram2d(X, Y, [x_axis, y_axis])
+# Compute the average
+mean = np.mean(crim_data)
+# Compute the standard deviation
+std = np.std(crim_data)
+# Covert the 2d array to a linear array, and sort in descending order
+linear_crim = np.sort(crim_data.flatten())[::-1]
+print(linear_crim)
+# Get the threshold index
+index = int(np.floor(len(linear_crim) * (100-89) / 100 -1))
+print(linear_crim[index])
+print(linear_crim[43])
 
 
-plt.show()
-for i in range(10):
-    print(shapeRecords[i].shape.__geo_interface__['coordinates'])
-'''
 
-mtr = gpd.GeoDataFrame.from_file(shpFilePath)
-ar = mtr.loc[:,'geometry'][7].__geo_interface__['coordinates']
-print(len(mtr.loc[:,'geometry']))
-print(ar[1])
-print(ar)
-#print(mtr.__geo_interface__('coordinates'))
+
+
+
+
+
 
